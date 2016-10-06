6c461a5dfe66587dce38b49f81c33411bd995122
ishaib <ishaib@il.ibm.com>
2016-10-06 16:17:01 +0300

    Init

---
+++ Example/Koloda/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Koloda
+//
+//  Created by Eugene Andreyev on 07/01/2015.
+//  Copyright (c) 07/01/2015 Eugene Andreyev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Example/Koloda/CardsViewController.swift
@@ -0,0 +1,111 @@
+//
+//  BackgroundAnimationViewController.swift
+//  Koloda
+//
+//  Created by Eugene Andreyev on 7/11/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import Koloda
+import pop
+
+private let numberOfCards: UInt = 5
+private let frameAnimationSpringBounciness: CGFloat = 9
+private let frameAnimationSpringSpeed: CGFloat = 16
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
+
+class CardsViewController: UIViewController {
+
+    weak var kolodaView: CustomKolodaView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
+        kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
+        kolodaView.delegate = self
+        kolodaView.dataSource = self
+        kolodaView.animator = CardsAnimator(koloda: kolodaView)
+        
+        self.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+    }
+    
+    
+    //MARK: IBActions
+    @IBAction func leftButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Left)
+    }
+    
+    @IBAction func rightButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Right)
+    }
+    
+    @IBAction func undoButtonTapped() {
+        kolodaView?.revertAction()
+    }
+}
+
+//MARK: KolodaViewDelegate
+extension CardsViewController: KolodaViewDelegate {
+    
+    func kolodaDidRunOutOfCards(koloda: KolodaView) {
+        kolodaView.resetCurrentCardIndex()
+    }
+    
+    func koloda(koloda: KolodaView, didSelectCardAtIndex index: UInt) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
+    }
+    
+    func kolodaShouldApplyAppearAnimation(koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func kolodaShouldMoveBackgroundCard(koloda: KolodaView) -> Bool {
+        return false
+    }
+    
+    func kolodaShouldTransparentizeNextCard(koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func koloda(kolodaBackgroundCardAnimation koloda: KolodaView) -> POPPropertyAnimation? {
+        let animation = POPSpringAnimation(propertyNamed: kPOPViewFrame)
+        animation.springBounciness = frameAnimationSpringBounciness
+        animation.springSpeed = frameAnimationSpringSpeed
+        return animation
+    }
+}
+
+//MARK: KolodaViewDataSource
+extension CardsViewController: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(koloda: KolodaView) -> UInt {
+        return numberOfCards
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardAtIndex index: UInt) -> UIView {
+        titleLabel.text = "cards_\(index + 1)"
+        var uiImage = UIImage (named: "cards_\(index + 1)")
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            let url = NSURL(string: "https://www.partycity.com/images/set_c/en_us/emails-pc/11-11-31-halloween-clearance-email/coupon-PCR3SP_v2.jpg")
+            let data = NSData(contentsOfURL: url!)
+            dispatch_async(dispatch_get_main_queue(), {
+                uiImage = UIImage(data: data!)
+            });
+        });
+        
+       
+        //return UIImageView(image: UIImage(data: data!)!)
+        return UIImageView(image: uiImage)
+    }
+    
+    func koloda(koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
+        return NSBundle.mainBundle().loadNibNamed("CustomOverlayView",
+            owner: self, options: nil)![0] as? OverlayView
+    }
+}
